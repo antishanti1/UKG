67ca1af4a70c1e13e1c3d0670d3a1d975229ed42
A Shutkova <ashutkova@As-MacBook-Pro.localdomain>
2023-05-05 12:43:50 -0400

    changing styles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import './App.scss';
 import Nav from '../src/components/Nav/Nav';
 import Dashboard from '../src/pages/Dashboard/Dashboard';
+import Header from "./components/Header/Header";
 // import calculatePsychSafetyScore from "../src/components/ChartInfo/ChartInfo";
 
 function App() {
   return (
-   <>
+
+ <div className="main">
     <Nav />
-    <Dashboard />
+    {/* <Dashboard /> */}
 
    <Routes>
    <Route path="/" element={<Dashboard />} />
    </Routes>
-   </>
+ </div> 
   );
 }
 
