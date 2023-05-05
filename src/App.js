5283923a1ec1485d82320d09b03aaa2a0b081e0a
AndrewVillalba <andrewvillalba1@gmail.com>
2023-05-05 10:23:21 -0400

    finished header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import { Route, Routes } from "react-router-dom";
 import './App.scss';
-import Nav from '../src/components/Nav/Nav'
+import Nav from '../src/components/Nav/Nav';
+import Dashboard from '../src/pages/Dashboard/Dashboard';
 
 function App() {
   return (
    <>
     <Nav />
+    <Dashboard />
 
    {/* <Routes>
     
