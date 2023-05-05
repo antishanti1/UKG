a15b489fc4f9f27fead7631ceab3f75f7d1053f7
A Shutkova <ashutkova@As-MacBook-Pro.localdomain>
2023-05-05 10:15:19 -0400

    added icons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,38 @@
 import './Nav.scss'
-// import ukglogo from '../../assets/icons/TeamWise Logo'
+import ukglogo from '../../assets/icons/TeamWiseLogo.svg'
+// import Dash from '../../assets/icons/ScheduleIcon.svg'
+import sch from '../../assets/icons/ScheduleIcon.svg'
+import emp from '../../assets/icons/EmployeesIcon.svg'
+import sch from '../../assets/icons/ScheduleIcon.svg'
+import sch from '../../assets/icons/ScheduleIcon.svg'
+import sch from '../../assets/icons/ScheduleIcon.svg'
 
 
 
 
 export default function Nav() {
     return (
-        <>
-        <h1>Nav</h1>
+        <> 
+        <div><img className="nav__logo" src={ukglogo} alt='ukg-logo'></img></div>
+      <div>
+        <h3>Dashboard</h3>
+      </div>
+      <div>
+        <h3>Scheduling</h3>
+      </div>
+      <div>
+        <h3>Emloyees</h3>
+      </div>
+      <div>
+        <h3>Results</h3>
+      </div>
+      <div>
+        <h3>Settings</h3>
+      </div>
+      <div>
+        <h3>Help</h3>
+      </div>
+   
         </>
     )
 } 
