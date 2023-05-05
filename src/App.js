937b3e4f548ee832f55e3773b5cc35a2d441422b
AndrewVillalba <andrewvillalba1@gmail.com>
2023-05-05 09:43:11 -0400

    commented out routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import './App.scss';
 
 function App() {
   return (
    <>
-   <Routes>
+   {/* <Routes>
     
-   </Routes>
+   </Routes> */}
    </>
   );
 }
