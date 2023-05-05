3d492a4deb1ccf438e7579df53af25240010c3be
A Shutkova <ashutkova@As-MacBook-Pro.localdomain>
2023-05-05 09:48:36 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import { Route, Routes } from "react-router-dom";
 import './App.scss';
+import Nav from '../src/components/Nav/Nav'
 
 function App() {
   return (
    <>
+    <Nav />
+
    {/* <Routes>
     
    </Routes> */}
