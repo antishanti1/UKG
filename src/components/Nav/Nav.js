e33709ad134be74a0ff4c377480fd25659e26b64
A Shutkova <ashutkova@As-MacBook-Pro.localdomain>
2023-05-05 12:25:03 -0400

    added links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import './Nav.scss'
+import { Link } from "react-router-dom";
 import ukglogo from '../../assets/icons/TeamWiseLogo.svg'
 import dunderMifflin from '../../assets/icons/dunder-mifflin-logo.svg'
 import dash from '../../assets/icons/dashboard-icon.svg'
@@ -21,8 +22,9 @@ export default function Nav() {
         <div className='nav__top'><img className="nav__logo" src={ukglogo} alt='ukg-logo'></img>
        <img className="nav__logo" src={dunderMifflin} alt='dunder-mifflin-logo'></img></div>
        <div className='nav__bottom'>
+       
       <div className='nav__sec'>
-      <img className="nav__sec-icon " src={dash} alt='dashboard-logo'></img> <h3> Dashboard</h3>
+      <img className="nav__sec-icon " src={dash} alt='dashboard-logo'></img>    <Link to = '/'><h3> Dashboard</h3></Link>
       </div>
       <div className='nav__sec'>
       <img className="nav__sec-icon" src={schedule} alt='scheduling-logo'></img> <h3>Scheduling</h3>
